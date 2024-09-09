2e6e731debc56d877e7ff257bf89a15e66487f0d
Glaucco Lima <glauccolima@gmail.com>
2024-09-09 13:54:54 -0300

    Implementando templates, views e corrigindo URL para redirecionamento

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import PostView
+from blog.views import PostView, PostDetailView  # Importando a view para detalhes do post
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect  # Importando redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django Admin habilitado
-    path('home/', PostView.as_view(), name='home-view'),
+    path('', lambda request: redirect('home-view')),  # Redireciona a raiz para /home/
+    path('home/', PostView.as_view(), name='home-view'),  # Página inicial (lista de posts)
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),  # Página de detalhes do post
 ]
 
 # Servir arquivos estáticos e de mídia durante o desenvolvimento
